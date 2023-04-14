928d154318d46ff241dde1255fbc9a281555b8e5
AlannGarcia <alanguillermogarcia2001@gmail.com>
2023-04-14 19:05:49 -0300

    agrego el login de clima app

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
-import express from 'express'
+const express = require('express');
+const path = require('path');
+const engine = require('ejs-mate');
+const morgan = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
 
-const app = express()
 
-app.listen(3000)
+//Inicializacion
+const app = express();
+require('./database');
+require('./passport/local.auth');
+
+
+//Configuraciones
+app.set('views', path.join(__dirname, 'views'));//informo donde se encuentran las vistas 
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));//para recibir los datos del cliente como en un registro
+app.use(session({
+    secret: 'contraSecreta',
+    resave: false,
+    saveUninitialized: false
+  })); //guardamos la sesion de los datos
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+//pongo el mensaje aca para tenerlo en toda la app
+app.use((req, res, next) => {
+    app.locals.errorDeRegistro = req.flash('errorDeRegistro');
+    app.locals.errorDeIngreso = req.flash('errorDeIngreso');
+    app.locals.user = req.user;//almaceno el usuario
+    next()
+});
+
+
+//Routes
+app.use('/', require('./routes/index'));
+
+
+//Iniciamos el servidor
+app.listen(3000, ()=>{
+    console.log(`Server on port 3000`);
+});
---
+++ routes/index.js
@@ -0,0 +1,46 @@
+const router = require('express').Router(); 
+const passport = require('passport');
+
+router.get('/', (req,res,next)=>{
+    res.render('index');
+});
+
+router.get('/signup', (req,res,next) =>{
+    res.render('signup');
+});
+
+router.post('/signup', passport.authenticate('local-signup',{
+    successRedirect: '/main',
+    failureRedirect: '/signup',
+    pastReqToCallBack: true 
+}));
+
+router.get('/signin', (req,res,next) =>{
+    res.render('signin');
+});
+
+router.post('/signin', passport.authenticate('local-signin', {
+    successRedirect: '/main',
+    failureRedirect: '/signin',
+    pastReqToCallBack: true 
+}));
+
+router.get('/main', isAuthenticated, (req,res,next) =>{
+    res.render('main');
+});
+
+router.get("/logout", (req, res, next) => {
+    req.logout(req.user, err => {
+      if(err) return next(err);
+      res.redirect("/");
+    });
+});
+
+function isAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/');
+}
+
+module.exports = router;
