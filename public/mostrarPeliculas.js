3537b3d4db97be3563baf45fce8cf5c49f91ed2a
AlannGarcia <alanguillermogarcia2001@gmail.com>
2023-04-28 20:30:00 -0300

    info de la pelicula

--- public/mostrarPeliculas.js
+++ public/mostrarPeliculas.js
@@ -20,3 +20,66 @@ function mostrarListaPeliculas() {
 }
 
 mostrarListaPeliculas()
+
+
+// Obtener la ventana emergente y los elementos de contenido
+const modal = document.getElementById("myModal");
+const modalPoster = document.getElementById("modalPoster");
+const modalTitle = document.getElementById("modalTitle");
+const modalSynopsis = document.getElementById("modalSynopsis");
+
+// Obtener todas las imágenes de portada y agregar un evento de clic
+const portadas = document.querySelectorAll("#pelis img");
+portadas.forEach((portada, index) => {
+  portada.addEventListener("click", () => {
+    // Obtener la información de la película correspondiente
+    const pelicula = peliculas[index];
+    
+    // Actualizar el contenido de la ventana emergente
+    modalPoster.src = pelicula.Poster;
+    modalTitle.innerText = pelicula.Titulo;
+    modalSynopsis.innerText = pelicula.Sinopsis;
+    
+    // Mostrar la ventana emergente
+    modal.style.display = "block";
+  });
+});
+
+const close = document.querySelector(".close");
+close.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+
+
+
+
+
+
+
+
+
+// const imagenes = document.querySelectorAll("#pelis img");
+// imagenes.forEach(img => {
+//   img.addEventListener("click", mostrarInformacion(img));
+// });
+
+
+// function mostrarInformacion(evento,img) {
+//     //const peli = peliculas.find(p => p.Poster == img);
+//     const peli = evento.target.dataset;
+//     console.log(peli);
+//     const titulo = document.getElementById("titulo");
+//     const sinopsis = document.getElementById("sinopsis");
+//     const trailer = document.getElementById("trailer");
+//     const puntuacion = document.getElementById("puntuacion");
+  
+//     //titulo.textContent = peli.Poster;
+//     //sinopsis.textContent = peli.Sinopsis;
+//     //trailer.textContent = peli.Trailer;
+//     //puntuacion.textContent = peli.Puntuacion;
+  
+//     const modal = document.getElementById("modal");
+//     modal.style.display = "block";
+//   }
+  
